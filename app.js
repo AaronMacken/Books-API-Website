9d51660224b57cc7e5b2c1dc43b8f128d03f9b60
Aaron Macken <aaron.a.macken@gmail.com>
2019-05-06 20:54:33 -0700

    Added some simple styling to the pages. Implemented the IT Book API to let users search for IT related books.

--- app.js
+++ app.js
@@ -6,24 +6,31 @@ let bodyParser = require("body-parser");
 
 app.set("view engine", "ejs");
 app.use(express.static("public"));
-app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.urlencoded({ extended: true }));
 
 app.get("/", (req, res) => {
-    res.render("landing");
+  res.render("landing");
 });
 
 app.get("/searchbook", (req, res) => {
-    res.render("searchbook");
+  key = req.query.key;
+  let URL = "https://api.itbook.store/1.0/search/" + key;
+
+  request(URL, (error, response, body) => {
+    if (!error && response.statusCode == 200) {
+      var info = JSON.parse(body);
+      console.log(info);
+      res.render("searchbook", { info: info });
+    }
+  });
 });
 
 app.get("/reviews", (req, res) => {
-    res.render("reviews");
+  res.render("reviews");
 });
 
 app.get("/postbook", (req, res) => {
-    res.render("postbook");
+  res.render("postbook");
 });
-
-
 
 app.listen(port, () => console.log("Server is listeneing"));
