365295d8e07b8e1785d71c2a24abef1253de3c69
Aaron Macken <aaron.a.macken@gmail.com>
2019-05-13 00:01:47 -0700

    Replaced the app.js Array with MongoDB to implement data persistence. Renamed several files to follow REST convention naming. Still need to update the 'write review' button under the searchbooks page.

--- app.js
+++ app.js
@@ -1,28 +1,33 @@
-let express = require("express");
-let app = express();
-let port = 3000;
-let request = require("request");
-let bodyParser = require("body-parser");
+const express = require("express"),
+  app = express(),
+  port = 3000,
+  request = require("request"),
+  bodyParser = require("body-parser"),
+  mongoose = require("mongoose");
 
 app.set("view engine", "ejs");
 app.use(express.static("public"));
 app.use(bodyParser.urlencoded({ extended: true }));
 
+// Mongo DB Setup //
+mongoose.connect("mongodb://localhost:27017/barneys", {
+  useNewUrlParser: true
+});
 
-let reviews = [
-  {
-    image: "https://images-na.ssl-images-amazon.com/images/I/51l5XzLln%2BL._SX348_BO1,204,203,200_.jpg",
-    name: "Cracking the Coding Interview",
-    review: "Great Book! I learned SO MUCH!"
-  }
-];
+const reviewSchema = new mongoose.Schema({
+  image: String,
+  name: String,
+  review: String
+});
 
+let Review = mongoose.model("Review", reviewSchema);
 
+// Landing Page //
 app.get("/", (req, res) => {
   res.render("landing");
 });
 
-
+// Search book API //
 app.get("/searchbook", (req, res) => {
   key = req.query.key;
   let URL = "https://api.itbook.store/1.0/search/" + key;
@@ -35,23 +40,63 @@ app.get("/searchbook", (req, res) => {
   });
 });
 
-
+// INDEX //
 app.get("/reviews", (req, res) => {
-  res.render("reviews", { reviews: reviews });
+  Review.find({}, function(err, reviews) {
+    if (err) {
+      console.log("Something went wrong when trying to fetch the reviews.");
+    } else {
+      res.render("index", { reviews: reviews });
+    }
+  });
 });
 
-app.get("/reviews/new", (req, res) => {
-  res.render("postbook");
-});
+// Initial addition to DB, added once.
+// Review.create({
+//   image:"https://images-na.ssl-images-amazon.com/images/I/51l5XzLln%2BL._SX348_BO1,204,203,200_.jpg",
+//   name: "Cracking the Coding Interview",
+//   review: "Great Book! I learned SO MUCH!"
+// }, function(err, newReview){
+//   if(err){
+//     console.log("Something went wrong creating the review!");
+//   }else{
+//     console.log("Added: "+ newReview.name);
+//   }
+// });
 
+// CREATE //
 app.post("/reviews", (req, res) => {
-  let review = {
-    image: req.body.image,
-    name: req.body.title,
-    review: req.body.review
-  }
-  reviews.push(review);
-  res.redirect("reviews");
+  Review.create(
+    {
+      image: req.body.image,
+      name: req.body.title,
+      review: req.body.review
+    },
+    function(err, newReview) {
+      if (err) {
+        console.log("Something went wrong when trying to post a review!");
+      } else {
+        console.log("Added a review for: " + newReview.name);
+        res.redirect("/reviews");
+      }
+    }
+  );
+});
+
+// NEW //
+app.get("/reviews/new", (req, res) => {
+  res.render("new");
+});
+
+// SHOW //
+app.get("/reviews/:id", (req, res) => {
+  Review.findById(req.params.id, (err, foundReview) => {
+    if (err) {
+      console.log("Something went wrong when loading the reviews");
+    } else {
+      res.render("show", { review: foundReview });
+    }
+  });
 });
 
 app.listen(port, () => console.log("Server is listeneing"));
