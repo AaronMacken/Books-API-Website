a3f024322c51a43874b288c6af2e73de25772d75
Aaron Macken <aaron.a.macken@gmail.com>
2019-05-19 15:27:58 -0700

    adding update and destroy functionality to the site.

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+// Require Variables
 const express = require("express"),
   app = express(),
   port = 3000,
@@ -5,29 +6,31 @@ const express = require("express"),
   bodyParser = require("body-parser"),
   mongoose = require("mongoose");
 
+
+// App Configure
 app.set("view engine", "ejs");
 app.use(express.static("public"));
 app.use(bodyParser.urlencoded({ extended: true }));
 
-// Mongo DB Setup //
+
+// Mongo DB Setup 
 mongoose.connect("mongodb://localhost:27017/barneys", {
   useNewUrlParser: true
 });
-
 const reviewSchema = new mongoose.Schema({
   image: String,
   name: String,
   review: String
 });
-
 let Review = mongoose.model("Review", reviewSchema);
+
 
 // Landing Page //
 app.get("/", (req, res) => {
   res.render("landing");
 });
 
-// Search book API //
+// api Index //
 app.get("/searchbook", (req, res) => {
   key = req.query.key;
   let URL = "https://api.itbook.store/1.0/search/" + key;
@@ -35,12 +38,13 @@ app.get("/searchbook", (req, res) => {
   request(URL, (error, response, body) => {
     if (!error && response.statusCode == 200) {
       var info = JSON.parse(body);
-      res.render("searchbook", { info: info });
+      console.log(req);
+      res.render("apiIndex", { info: info });
     }
   });
 });
 
-// INDEX //
+// review Index //
 app.get("/reviews", (req, res) => {
   Review.find({}, function(err, reviews) {
     if (err) {
@@ -50,6 +54,53 @@ app.get("/reviews", (req, res) => {
     }
   });
 });
+
+
+// Create
+app.post("/reviews", (req, res) => {
+
+  Review.create(req.body.post,
+    function(err, newReview) {
+      if (err) {
+        console.log("Something went wrong when trying to post a review!");
+      } else {
+        console.log("Added a review for: " + newReview.name);
+        res.redirect("/reviews");
+      }
+    }
+  );
+});
+
+// New
+app.get("/reviews/new", (req, res) => {
+  var data = "";
+  res.render("new", {data: data});
+});
+
+// Show
+app.get("/reviews/:id", (req, res) => {
+  Review.findById(req.params.id, (err, foundReview) => {
+    if (err) {
+      console.log("Something went wrong when loading the reviews");
+    } else {
+      res.render("show", { review: foundReview });
+    }
+  });
+});
+
+//Edit
+app.get("/reviews/:id/edit", (req, res) => {
+  Review.findById(req.params.id, (err, foundReview) => {
+    if(err){
+      res.redirect("/reviews")
+    }else{
+      res.render("edit", {review: foundReview});
+    }
+  });
+});
+
+
+app.listen(port, () => console.log("Server is listeneing"));
 
 // Initial addition to DB, added once.
 // Review.create({
@@ -63,40 +114,3 @@ app.get("/reviews", (req, res) => {
 //     console.log("Added: "+ newReview.name);
 //   }
 // });
-
-// CREATE //
-app.post("/reviews", (req, res) => {
-  Review.create(
-    {
-      image: req.body.image,
-      name: req.body.title,
-      review: req.body.review
-    },
-    function(err, newReview) {
-      if (err) {
-        console.log("Something went wrong when trying to post a review!");
-      } else {
-        console.log("Added a review for: " + newReview.name);
-        res.redirect("/reviews");
-      }
-    }
-  );
-});
-
-// NEW //
-app.get("/reviews/new", (req, res) => {
-  res.render("new");
-});
-
-// SHOW //
-app.get("/reviews/:id", (req, res) => {
-  Review.findById(req.params.id, (err, foundReview) => {
-    if (err) {
-      console.log("Something went wrong when loading the reviews");
-    } else {
-      res.render("show", { review: foundReview });
-    }
-  });
-});
-
-app.listen(port, () => console.log("Server is listeneing"));
